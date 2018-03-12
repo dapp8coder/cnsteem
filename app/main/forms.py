28ab70910c35275e31476987ba4a8e46a0047a10
skenan <skenan1992@gmail.com>
2018-03-11 21:14:14 -0700

    remove confirmed email check

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,8 +9,6 @@ class PaymentForm(FlaskForm):
                                               Regexp('^[a-z][a-z0-9.-]*[a-z0-9]$', 0,
                                                      '用户名必须由小写字母，0-9，破折号或点号构成，首字符必须为字母，必须以字母或数字结尾')])
     email = StringField('Email', validators=[Email('Email不合法'), Length(1, 64, '长度必须在1和64之间')])
-    confirmed_email = StringField('确认Email',
-                                  validators=[Email('Email不合法'), EqualTo('confirmed_email', message='Email必须相同')])
     amount = StringField('金额', render_kw={'readonly': True}, default="$2.00")
     submit = SubmitField('支付宝付款')
     submit_wepay = SubmitField('微信付款')
