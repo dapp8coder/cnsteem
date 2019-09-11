9d51233aa8e79bcc140c2e695e39acf646f07849
skenan <skenan1992@gmail.com>
2019-09-11 16:33:09 -0700

    Adjust text

--- README.md
+++ README.md
@@ -4,10 +4,6 @@
 
 Flask + Beem API + Vmq
 
-### 交流社区
-
-[Makeet.io](https://makeet.io)
-
 ### 特别鸣谢
 
 [Vmq](https://github.com/szvone/Vmq) - 个人开发者收款解决方案
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,6 +4,6 @@ import os
 
 main = Blueprint('main', __name__)
 
-steem_tool = Steem(keys=[os.environ['STEEM_ACTIVE_KEY']])
+steem_tool = Steem(node='http://anyx.io/', keys=[os.environ['STEEM_ACTIVE_KEY']])
 
 from . import views, errors
